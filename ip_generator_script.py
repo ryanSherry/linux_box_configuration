5785822d14fa49ca5d571dd194f9c7579b18d4be
Ryan Sherry <rsherrycomputers@gmail.com>
2023-10-07 15:14:28 -0700

    updated script to write to file

--- ip_generator_script.py
+++ ip_generator_script.py
@@ -1,4 +1,4 @@
-import sys
+import sys, webbrowser
 
 ip = sys.argv[1]
 port = sys.argv[2]
@@ -7,5 +7,18 @@ password = sys.argv[4]
 
 ipArray = ip.split(".")
 
-for number in range(int(ipArray[3])+2, int(ipArray[3])+15):
-	print(f'{ipArray[0]}.{ipArray[1]}.{ipArray[2]}.{number}:{port}:{user}:{password}')
+original_stdout = sys.stdout
+
+with open('usable_ips.txt', 'a+') as file:
+	sys.stdout = file
+	
+	print(f'For {ip}:')
+	print('')
+
+	for number in range(int(ipArray[3])+2, int(ipArray[3])+15):
+		print(f'{ipArray[0]}.{ipArray[1]}.{ipArray[2]}.{number}:{port}:{user}:{password}')
+	
+	print('')
+
+	sys.stdout = original_stdout
+file.close()
